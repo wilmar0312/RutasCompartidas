8a09b02f3641e64627650d0d896f7c905d808f8a
wilmar sanchez <wilmarsanchez347@gmail.com>
2025-03-22 21:39:58 -0400

    Login implementation

---
+++ RutasCompartidas.Application/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using RutasCompartidas.Domain.Entities;
+using RutasCompartidas.Infrastructure.Persistence;
+
+namespace RutasCompartidas.Application.Services
+{
+    public class AuthService
+    {
+        private readonly AppDbContext _context;
+
+        public AuthService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Usuario?> LoginAsync(string email, string password)
+        {
+            return await _context.Usuarios
+                .FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+        }
+    }
+}
