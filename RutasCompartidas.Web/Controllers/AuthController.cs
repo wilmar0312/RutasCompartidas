a9cb41261ed2620394b4ecea288a06ab9f7c36fb
wilmar sanchez <wilmarsanchez347@gmail.com>
2025-03-22 21:58:20 -0400

    Add Controller

---
+++ RutasCompartidas.Web/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RutasCompartidas.Application.Services;
+
+namespace RutasCompartidas.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var user = await _authService.LoginAsync(email, password);
+            if (user != null)
+            {
+                HttpContext.Session.SetString("UserId", user.Id.ToString());
+                HttpContext.Session.SetString("UserRole", user.Rol);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Credenciales incorrectas";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
