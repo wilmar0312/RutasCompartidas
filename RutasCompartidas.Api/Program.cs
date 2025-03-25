149c69055eb7cc33d8a451f004ba976471a46e2b
wilmar sanchez <wilmarsanchez347@gmail.com>
2025-03-24 23:11:18 -0400

    I Add the Api

--- RutasCompartidas.Api/Program.cs
+++ RutasCompartidas.Api/Program.cs
@@ -1,25 +1,35 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configurar conexión a SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Registrar servicios de aplicación
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<RutaService>();
+
+// Habilitar CORS (para permitir que otros clientes consuman la API)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// Usar CORS
+app.UseCors("AllowAll");
+
+// Habilitar Swagger
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
