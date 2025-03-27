ca72da3a884443c68c86a43e13f0e8a3255a6aa1
wilmar sanchez <wilmarsanchez347@gmail.com>
2025-03-26 20:22:44 -0400

    Homework No.6

---
+++ RutasCompartidas.Infrastructure/Exceptions/RutaException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RutasCompartidas.Infrastructure.Exceptions
+{
+    internal class RutaException
+    {
+    }
+}
